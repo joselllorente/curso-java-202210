ae1674158beaaa788a51f57761b0ade40021e3ea
Jose Luis Llorente <joseluis.llorenteperales@gmail.com>
2022-10-28 11:40:30 +0200

    Ejemplos con Set

--- src/curso/java/colecciones/ListasJava.java
+++ src/curso/java/colecciones/ListasJava.java
@@ -1,18 +1,20 @@
 package curso.java.colecciones;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListasJava {
 
 	public static void main(String[] args) {
 		
-		ArrayList<String> lista1 = new ArrayList<String>();
+		List<String> lista1 = new ArrayList<String>();
+		
 		lista1.add("Hola");
 		lista1.add("Hola");
 //		lista1.add(1);
 //		lista1.add(new Boton("",""));
 		lista1.add("qué");
-		lista1.add("tal?");
+		lista1.add("tal?"); 
 		lista1.add(1, "hoy es viernes por fin!!!");
 		
 		System.out.println(lista1.size());
@@ -25,6 +27,9 @@ public class ListasJava {
 		for (String texto : lista1) {
 			System.out.println(texto);
 		}
+		
+		lista1.remove(0);
+		lista1.remove("Hola");
 	}
 
 }
--- src/curso/java/inicio/StringEquals.java
+++ src/curso/java/inicio/StringEquals.java
@@ -5,11 +5,13 @@ public class StringEquals {
 	public static void main(String[] args) {
 		String a = "1";
 		String b = "Hola";
+		System.out.println(b.hashCode());
 		String c = new String("Hola");
+		System.out.println(c.hashCode());
 		System.out.println(a.equals(b));
 		System.out.println(a==b);
-		System.out.println(a==c);	
-		System.out.println(a.equals(c));
+		System.out.println(b==c);	
+		System.out.println(b.equals(c));
 
 	}
 
