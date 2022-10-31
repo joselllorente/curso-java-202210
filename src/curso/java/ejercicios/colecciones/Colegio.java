11a6e6e8d8de7d95f678af74cda4d3ee5ef8662e
Jose Luis Llorente <joseluis.llorenteperales@gmail.com>
2022-10-31 12:11:20 +0100

    Solucion Ejercicio Colecciones

---
+++ src/curso/java/ejercicios/colecciones/Colegio.java
@@ -0,0 +1,118 @@
+package curso.java.ejercicios.colecciones;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Colegio {
+	
+	private List<Estudiante> estudiantes;
+
+	public Colegio() {
+		super();
+		this.estudiantes = new ArrayList<Estudiante>();
+	}
+	
+	public static void main(String[] args) {
+		Colegio colegio = new Colegio();
+		colegio.inciaColegio();
+		
+	}
+	
+	private void inciaColegio() {
+		crearEstudiantes();
+		mostrarDatosEstudiantes();
+//		mostrarDatosEstudiantesMatematicas();
+//		mostrarDatosEstudiantesAprobados();
+		eliminaSuspensosLengua();
+		mostrarDatosEstudiantes();
+	}
+	
+	private void mostrarDatosEstudiantes() {
+		for (Estudiante estudiante : estudiantes) {
+			System.out.println(estudiante);
+			for (Asignatura asignatura : estudiante.getAsignaturas()) {
+				System.out.println(asignatura);
+			}
+		}
+		
+	}
+	
+	
+	private void mostrarDatosEstudiantesMatematicas() {
+		for (Estudiante estudiante : estudiantes) {
+			boolean tieneMatematicas = false;
+			for (Asignatura asignatura : estudiante.getAsignaturas()) {
+				if(asignatura.getNombre().equals("Matematicas")) {
+					tieneMatematicas = true;
+				}
+				
+			}
+			if (tieneMatematicas) {
+				System.out.println(estudiante);
+			}
+		}
+	}
+	
+	
+	private void mostrarDatosEstudiantesAprobados() {
+		for (Estudiante estudiante : estudiantes) {
+			double sumanotas = 0;
+			for (Asignatura asignatura : estudiante.getAsignaturas()) {
+				sumanotas += asignatura.getNota();
+			}
+			
+			if (sumanotas/estudiante.getAsignaturas().size() >= 5) {
+				System.out.println("Alumno aprobado con "+(sumanotas/estudiante.getAsignaturas().size()));
+				System.out.println(estudiante);
+			}
+		}
+		
+	}
+	
+	
+	private void eliminaSuspensosLengua() {
+		System.out.println("Eliminando alumnos");
+		for (Estudiante estudiante : estudiantes) {
+			System.out.println("Comprobando los datos para "+estudiante.getNombre());
+			for (Asignatura asignatura : estudiante.getAsignaturas()) {
+				if(asignatura.getNombre().equals("Lengua") && asignatura.getNota()<5 ) {
+					estudiantes.remove(estudiante);
+				}
+			}
+			System.out.println("Termina con "+estudiante.getNombre());
+		}
+		System.out.println("Finaliza eliminaSuspensosLengua");
+	}
+	
+	private void crearEstudiantes() {
+		//Alumno1
+		Asignatura asignatura1Alumno1 = new Asignatura ("Lengua", 3);
+		Asignatura asignatura2Alumno1 = new Asignatura ("Matematicas", 4);
+		Asignatura asignatura3Alumno1 = new Asignatura ("Fisica", 10);
+		
+		Set <Asignatura> asignaturasAlumno1 = new HashSet <Asignatura>();
+		asignaturasAlumno1.add(asignatura1Alumno1);
+		asignaturasAlumno1.add(asignatura2Alumno1);
+		asignaturasAlumno1.add(asignatura3Alumno1);
+		
+		Estudiante estudiante1 = new Estudiante("pepe", "brbrbr", "1111111A", asignaturasAlumno1);
+		estudiantes.add(estudiante1);
+		
+		
+		//Alumno2
+		Asignatura asignatura1Alumno2 = new Asignatura ("Lengua", 3);
+		Asignatura asignatura2Alumno2 = new Asignatura ("Matematicas", 8);
+		Asignatura asignatura3Alumno2 = new Asignatura ("Fisica", 6);
+		
+		Set <Asignatura> asignaturasAlumno2 = new HashSet <Asignatura>();
+		asignaturasAlumno2.add(asignatura1Alumno2);
+		asignaturasAlumno2.add(asignatura2Alumno2);
+		asignaturasAlumno2.add(asignatura3Alumno2);
+		
+		Estudiante estudiante2 = new Estudiante("Ramon", "Apellidos", "2222222B", asignaturasAlumno2);
+		estudiantes.add(estudiante2);
+	}
+
+}
---
+++ src/curso/java/ejercicios/colecciones/Estudiante.java
@@ -0,0 +1,65 @@
+package curso.java.ejercicios.colecciones;
+
+import java.util.Set;
+
+public class Estudiante {
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	private Set<Asignatura> asignaturas;
+	
+	public Estudiante(String nombre, String apellidos, String dni, Set<Asignatura> asignaturas) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+		this.asignaturas = asignaturas;
+	}
+	
+	public Estudiante(String nombre, String apellidos, String dni) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public Set<Asignatura> getAsignaturas() {
+		return asignaturas;
+	}
+
+	public void setAsignaturas(Set<Asignatura> asignaturas) {
+		this.asignaturas = asignaturas;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [nombre=" + nombre + ", apellidos=" + apellidos + ", dni=" + dni +"]";
+	}
+	
+	
+	
+}
