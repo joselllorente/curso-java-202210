a4ed24f1602260f4a028e50f65bc62d689b667a0
Jose Luis Llorente <joseluis.llorenteperales@gmail.com>
2022-10-31 13:31:29 +0100

    Modificacion funcionalidad

--- src/curso/java/inicio/PruebaGit.java
+++ src/curso/java/inicio/PruebaGit.java
@@ -7,7 +7,7 @@ public class PruebaGit {
 		System.out.println("Hola mundo2");
 		System.out.println("Hola mundo3");
 		System.out.println("Hola mundo4");
-		System.out.println("Hola mundo5");
+		System.out.println("Hola mundo6");
 	}
 
 }
