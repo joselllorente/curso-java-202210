49634630852e837967b4f9d90360896bd9bb8c48
Jose Luis <joseluis.llorenteperales@gmail.com>
2022-10-31 13:22:55 +0100

    Update Test.java

--- src/curso/java/inicio/Test.java
+++ src/curso/java/inicio/Test.java
@@ -3,7 +3,7 @@ package curso.java.inicio;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Empezando2");
+		System.out.println("Empezando3");
 		String texto= "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
 				+ "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
 				+ "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
